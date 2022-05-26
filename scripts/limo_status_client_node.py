afb54da8c7f977b8e6c23098743e6d13569a447b
hanskw4267 <hans.kurnia@westonrobot.com>
2022-05-26 17:22:43 +0800

    init empty list and assign instead

--- scripts/limo_status_client_node.py
+++ scripts/limo_status_client_node.py
@@ -6,14 +6,14 @@ from std_msgs.msg import String
 
 def publish_status_strings():
     # To store our publishers
-    publishers = []
+    publishers = [None] * 5
 
     # Create and initialize the publishers using the srv constants
-    publishers.append(rospy.Publisher("/limo_status/vehicle_state", String, queue_size=1))
-    publishers.append(rospy.Publisher("/limo_status/control_mode", String, queue_size=1))
-    publishers.append(rospy.Publisher("/limo_status/battery_voltage", String, queue_size=1))
-    publishers.append(rospy.Publisher("/limo_status/error_code", String, queue_size=1))
-    publishers.append(rospy.Publisher("/limo_status/motion_mode", String, queue_size=1))
+    publishers[GetLimoStatusRequest.GET_STATUS_VEHICLE_STATE] = rospy.Publisher("/limo_status/vehicle_state", String, queue_size=1)
+    publishers[GetLimoStatusRequest.GET_STATUS_CONTROL_MODE] = rospy.Publisher("/limo_status/control_mode", String, queue_size=1)
+    publishers[GetLimoStatusRequest.GET_STATUS_BATTERY_VOLTAGE] = rospy.Publisher("/limo_status/battery_voltage", String, queue_size=1)
+    publishers[GetLimoStatusRequest.GET_STATUS_ERROR_CODE] = rospy.Publisher("/limo_status/error_code", String, queue_size=1)
+    publishers[GetLimoStatusRequest.GET_STATUS_MOTION_MODE] = rospy.Publisher("/limo_status/motion_mode", String, queue_size=1)
 
     # Get the service function
     get_status_string = rospy.ServiceProxy("get_limo_status", GetLimoStatus)
